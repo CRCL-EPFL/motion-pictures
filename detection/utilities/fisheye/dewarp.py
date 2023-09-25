b3cf423b6e4766bb5b55dbcebbb670783dd3cffa
Eric Duong <eduong@proton.me>
2023-09-25 15:36:27 +0200

    Reorganize detection utilities and make yolov8Pose

--- detection/utilities/fisheye/dewarp.py
+++ detection/utilities/fisheye/dewarp.py
@@ -12,6 +12,9 @@ def undistort(img_path):
     
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)    
+    # Comment out if image doesn't need to be rotated
+    # undistorted_img = cv2.rotate(undistorted_img, cv2.ROTATE_180)
+
     
     cv2.imshow("undistorted", undistorted_img)
     cv2.imwrite("imgundistort.jpg", undistorted_img)
